59af1c598bc9dcb7cdb2fc494afd4845b5030b98
Zac Walters <zacwalters4@gmail.com>
2022-08-03 14:26:48 -0600

    Complete challenge 2

--- for.js
+++ for.js
@@ -12,6 +12,10 @@ for (i = 26; i < 76; i+=2){
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+for (i = 0; i < weather_options.length; i++){
+  console.log(`Today's weather is ${weather_options[i]}`)
+}
+
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
